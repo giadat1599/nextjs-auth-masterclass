1932448f90bc0a8796fa8826e83a1df17e711285
Gia Dat Truong <giadattruong@Gias-MacBook-Pro.local>
2024-01-14 14:16:10 +0700

    feat: callbacks

---
+++ next-auth.d.ts
@@ -0,0 +1,8 @@
+import { UserRole } from "@prisma/client";
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+   interface Session {
+      user: { role: UserRole } & DefaultSession["user"];
+   }
+}
