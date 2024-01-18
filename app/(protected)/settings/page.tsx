815084ab048627038b39d45ab6fdf67d2929218d
Gia Dat Truong <giadattruong@192.168.2.5>
2024-01-18 22:25:05 +0700

    feat: button and hooks

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,19 +1,12 @@
-import { auth, signOut } from "@/auth";
+"use client";
 
-export default async function SettingsPage() {
-   const session = await auth();
+import { logout } from "@/actions/logout";
 
+export default function SettingsPage() {
+   const onClick = () => logout();
    return (
-      <div>
-         {JSON.stringify(session)}
-         <form
-            action={async () => {
-               "use server";
-               await signOut();
-            }}
-         >
-            <button type="submit">Sign out</button>
-         </form>
+      <div className="bg-white p-10 rounded-xl">
+         <button onClick={onClick}>Sign out</button>
       </div>
    );
 }
